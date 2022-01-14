d53ae3696575c895cf037a1f49da314af673d462
NickyGuants <nicholasguantai528@gmail.com>
2022-01-14 11:56:33 +0300

    feat:insert new array elements

--- src/main/java/com/nicky/Arrays/Array.java
+++ src/main/java/com/nicky/Arrays/Array.java
@@ -4,14 +4,54 @@ public class Array {
     private int[] items;
     private int count;
 
-    public Array(int lenghth){
-        items= new int[lenghth];
+    public Array(int length){
+        items= new int[length];
+        count=0;
     }
 
-    //methods to manaupulate the array
-
+    //methods to manipulate the Dynamic array
+    //insert an element at the end of the array
     public void insert(int value){
+        //check if array is full
+        if(isFull()){
+           createNewArray();
+        }
+        items[count]=value;
+        count++;
+    }
 
+    //Insert an element at the middle(anywhere but the last index)
+    public void insertMiddle(int value, int index){
+        if (isFull()){
+            createNewArray();
+        }
+        for(int i=count; i>index; i--){
+            items[i]=items[i-1];
+        }
+        items[index]=value;
+        count++;
+    }
+
+    //private methods to check if the array is full and creating a new larger array.
+    private Boolean isFull(){
+        return count == items.length;
+    }
+    private void createNewArray(){
+        int [] newItems= new int[count*2];
+        //copy elements from old array to new array
+        for(int i=0; i< items.length; i++){
+            newItems[i]=items[i];
+        }
+        items=newItems;
+    }
+
+    //implement a method to print out the array elements
+    public void print(){
+        //Iterate over the array and print each element
+        //Time complexity = 0(n)
+        for(int i =0; i< items.length; i++){
+            System.out.println(items[i]);
+        }
     }
 
 }
--- src/main/java/com/nicky/Arrays/Main.java
+++ src/main/java/com/nicky/Arrays/Main.java
@@ -2,6 +2,16 @@ package com.nicky.Arrays;
 
 public class Main {
     public static void main (String[] args){
-        Array numbers = new Array(10);
+        //Testing the insert method
+        Array numbers = new Array(1);
+        numbers.insert(10);
+        numbers.insert(20);
+        numbers.insertMiddle(15,1);
+        numbers.insertMiddle(5, 0);
+        numbers.insert(25);
+
+
+
+        numbers.print();
     }
 }
