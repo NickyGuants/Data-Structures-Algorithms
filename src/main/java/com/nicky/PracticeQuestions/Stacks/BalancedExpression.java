6f1feed59f404dd15e5d229989c82c905b9062b2
NickyGuants <nicholasguantai528@gmail.com>
2022-01-27 09:57:05 +0300

    feat:balanced expression problem

--- src/main/java/com/nicky/PracticeQuestions/Stacks/BalancedExpression.java
+++ src/main/java/com/nicky/PracticeQuestions/Stacks/BalancedExpression.java
@@ -1,2 +1,28 @@
-package com.nicky.PracticeQuestions.Stacks;public class BalancedExpression {
+package com.nicky.PracticeQuestions.Stacks;
+
+import java.util.Stack;
+
+public class BalancedExpression {
+
+    public Boolean isBalancedExpression(String str){
+        Stack<Character> stack = new Stack<>();
+
+       for(int i=0; i<str.length(); i++){
+           if(str.charAt(i)=='('){
+               stack.push(str.charAt(i));
+           }
+           if(str.charAt(i) == ')'){
+               if (stack.empty()) return false;
+               char top =stack.pop();
+               if( top != '(')
+                   return false;
+           }
+       }
+       return stack.empty();
+    }
+
+    public static void main(String [] args){
+        BalancedExpression balancedExpression = new BalancedExpression();
+        System.out.println(balancedExpression.isBalancedExpression("(((1+2))))"));
+    }
 }
