b37d360d7daf3027c71e8fa669550fd9893c0ff9
NickyGuants <nicholasguantai528@gmail.com>
2022-02-03 09:36:34 +0300

    feat:reverse array using single and two pointers

---
+++ src/main/java/com/nicky/PracticeQuestions/Arrays/ReverseArray.java
@@ -0,0 +1,43 @@
+package com.nicky.PracticeQuestions.Arrays;
+
+import java.util.Arrays;
+
+public class ReverseArray {
+    //using a single pointer
+    public static void reverse(int [] nums){
+        int temp;
+        for(int i=0; i<nums.length/2; i++){
+            temp =nums[i];
+            nums[i]=nums[nums.length-i-1];
+            nums[nums.length-i-1]=temp;
+        }
+    }
+
+    //using two pointers
+    public static void reverseTwoPointer(int [] nums){
+        int temp;
+        int i=0;
+        int j= nums.length-1;
+
+        while (i<j){
+            swap(nums, i, j);
+            i++;
+            j--;
+        }
+    }
+
+    public static void swap(int[] nums, int i, int j){
+        int temp=nums[i];
+        nums[i]=nums[j];
+        nums[j]=temp;
+    }
+
+
+    public static void main(String [] args){
+        int [] nums= {1,2,3,4,5,6};
+        //ReverseArray.reverse(nums);
+        //System.out.println(Arrays.toString(nums));
+        ReverseArray.reverseTwoPointer(nums);
+        System.out.println(Arrays.toString(nums));
+    }
+}
