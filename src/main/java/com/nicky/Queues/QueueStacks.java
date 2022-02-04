9609ce8193ba3c174351a53fb0ff5f95d094a888
NickyGuants <nicholasguantai528@gmail.com>
2022-02-04 10:23:44 +0300

    feat:implement queue using two stacks

--- src/main/java/com/nicky/Queues/QueueStacks.java
+++ src/main/java/com/nicky/Queues/QueueStacks.java
@@ -1,2 +1,32 @@
-package com.nicky.Queues;public class QueueStacks {
+package com.nicky.Queues;
+
+import java.util.Stack;
+
+public class QueueStacks {
+    //create two stacks
+    private Stack<Integer> stack1 = new Stack<>();
+    private Stack<Integer> stack2= new Stack<>();
+
+    //we add to the first stack
+    //Time complexity O(1)
+    public void enqueue(int item){
+        stack1.push(item);
+    }
+
+    //Time complexity O(n)
+    public int dequeue(){
+        //the queue is empty
+        if (isEmpty()) throw new IllegalStateException();
+
+        if (stack2.isEmpty()){
+            while (!stack1.empty()){
+                stack2.push(stack1.pop());
+            }
+        }
+        return stack2.pop();
+    }
+
+    public Boolean isEmpty(){
+        return stack1.isEmpty() && stack2.isEmpty();
+    }
 }
