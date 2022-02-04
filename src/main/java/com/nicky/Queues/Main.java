d9e0a6ea60d2a54814ab38f3c8617b34d58ab44d
NickyGuants <nicholasguantai528@gmail.com>
2022-02-04 10:25:05 +0300

    feat:testing queues

--- src/main/java/com/nicky/Queues/Main.java
+++ src/main/java/com/nicky/Queues/Main.java
@@ -7,19 +7,33 @@ import java.util.Stack;
 public class Main {
     public static void main(String [] args){
         Queue<Integer> queue = new ArrayDeque<>();
-        ArrayQueue queue1 = new ArrayQueue(5);
+        ArrayQueue queue1 = new ArrayQueue(3);
+        QueueStacks queueStacks = new QueueStacks();
+        PriorityQueues priorityQueues = new PriorityQueues(3);
 
         queue1.enqueue(10);
         queue1.enqueue(20);
         queue1.enqueue(30);
+//        System.out.println( queue1.dequeue());
+//        queue.add(10);
+//        queue.add(20);
+//        queue.add(30);
+//        Main.reverse(queue);
+//        System.out.println(queue1);
+//        System.out.println(queue1.isEmpty());
+//        System.out.println(queue1.peek());
+//        System.out.println(queue1.isFull());
 
-        System.out.println( queue1.dequeue());
-        queue.add(10);
-        queue.add(20);
-        queue.add(30);
-        Main.reverse(queue);
+//        queueStacks.enqueue(10);
+//        queueStacks.enqueue(20);
+//        queueStacks.enqueue(30);
+//        var first = queueStacks.dequeue();
+//        System.out.println(first);
+        priorityQueues.enqueue(10);
+        priorityQueues.enqueue(30);
+        priorityQueues.enqueue(20);
 
-        System.out.println(queue1);
+        System.out.println(priorityQueues);
     }
 
     public static void reverse(Queue<Integer> queue){
