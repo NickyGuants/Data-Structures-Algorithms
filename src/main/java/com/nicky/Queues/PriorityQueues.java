c52e9b2ca6bb1f5f970c523fdda5a26a95cd7aa0
NickyGuants <nicholasguantai528@gmail.com>
2022-02-04 10:24:25 +0300

    feat:implement a priority queue using an array

--- src/main/java/com/nicky/Queues/PriorityQueues.java
+++ src/main/java/com/nicky/Queues/PriorityQueues.java
@@ -1,2 +1,31 @@
-package com.nicky.Queues;public class PriorityQueues {
+package com.nicky.Queues;
+
+import java.util.Arrays;
+
+public class PriorityQueues {
+    private int[] items;
+    private int count;
+
+    public PriorityQueues(int capacity){
+        items=new int[capacity];
+    }
+
+    public void enqueue(int item){
+        //shifting elements
+        int i;
+        for (i=count-1; i>=0; i--){
+            if (items[i] > item){
+                items[i+1]=items[i];
+            }else{
+                break;
+            }
+        }
+        items[i+1]=item;
+        count++;
+    }
+
+    @Override
+    public String toString(){
+        return Arrays.toString(items);
+    }
 }
