3f4e9112e19acc8ac30fe5500ff63719babb2564
NickyGuants <nicholasguantai528@gmail.com>
2022-02-08 11:15:05 +0300

    feat:check if expressions are balanced using a stack

--- src/main/java/com/nicky/PracticeQuestions/Stacks/BalancedExpression.java
+++ src/main/java/com/nicky/PracticeQuestions/Stacks/BalancedExpression.java
@@ -8,21 +8,28 @@ public class BalancedExpression {
         Stack<Character> stack = new Stack<>();
 
        for(int i=0; i<str.length(); i++){
-           if(str.charAt(i)=='('){
+           if(str.charAt(i)=='(' || str.charAt(i)=='{'){
                stack.push(str.charAt(i));
            }
-           if(str.charAt(i) == ')'){
+           if(str.charAt(i) == ')' || str.charAt(i) == '}' || str.charAt(i)==']'){
                if (stack.empty()) return false;
                char top =stack.pop();
-               if( top != '(')
+               if( (str.charAt(i)==')' && top != '(') || (str.charAt(i)=='}' && top != '{') || (str.charAt(i)==']' && top != '['))
                    return false;
            }
        }
        return stack.empty();
     }
 
+//    public Boolean match(char ch){
+//        if (ch=='')
+//    }
+
     public static void main(String [] args){
         BalancedExpression balancedExpression = new BalancedExpression();
-        System.out.println(balancedExpression.isBalancedExpression("(((1+2))))"));
+        System.out.println(balancedExpression.isBalancedExpression("()"));
+        System.out.println(balancedExpression.isBalancedExpression("({))"));
+        System.out.println(balancedExpression.isBalancedExpression("({))"));
+        System.out.println(balancedExpression.isBalancedExpression("()[]{}"));
     }
 }
