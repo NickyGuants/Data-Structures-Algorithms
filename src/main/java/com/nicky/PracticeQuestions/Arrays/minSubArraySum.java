d29e8464dc566e234a7054d8ae7396ad4e7872c7
NickyGuants <nicholasguantai528@gmail.com>
2022-02-08 11:16:00 +0300

    feat:find sum of the min sub-array using sliding window

--- src/main/java/com/nicky/PracticeQuestions/Arrays/minSubArraySum.java
+++ src/main/java/com/nicky/PracticeQuestions/Arrays/minSubArraySum.java
@@ -1,2 +1,12 @@
-package com.nicky.PracticeQuestions.Arrays;public class minSubArraySum {
+package com.nicky.PracticeQuestions.Arrays;
+
+public class minSubArraySum {
+    public static int minSubArrayLen(int target, int[] nums){
+       return 0;
+    }
+
+    public static void main(String [] args){
+        int [] nums={2,3,1,2,4,3};
+        System.out.println(minSubArrayLen(7, nums));
+    }
 }
