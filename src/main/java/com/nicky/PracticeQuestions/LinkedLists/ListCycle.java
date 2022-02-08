072def39d1d119d7b05190c662914d4ea824d78c
NickyGuants <nicholasguantai528@gmail.com>
2022-02-08 11:14:26 +0300

    feat:check if linked list has a cycle

--- src/main/java/com/nicky/PracticeQuestions/LinkedLists/ListCycle.java
+++ src/main/java/com/nicky/PracticeQuestions/LinkedLists/ListCycle.java
@@ -1,2 +1,28 @@
-package com.nicky.PracticeQuestions.LinkedLists;public class ListCycle {
+package com.nicky.PracticeQuestions.LinkedLists;
+
+import java.util.HashMap;
+
+public class ListCycle {
+    class ListNode {
+      int val;
+      ListNode next;
+      ListNode(int x) {
+          val = x;
+          next = null;
+      }}
+
+    public Boolean hasCycle (ListNode head){
+        HashMap<Integer, ListNode> map = new HashMap<>();
+
+        while (head !=null){
+            map.put(head.val, head.next);
+            head=head.next;
+        }
+
+    return false;
+    }
+
+    public static void main(String [] args){
+        ListCycle listCycle = new ListCycle();
+    }
 }
