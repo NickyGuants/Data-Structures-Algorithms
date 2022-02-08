91a88ace479b3fcbbf48f5d41e7c29fcac721ad2
NickyGuants <nicholasguantai528@gmail.com>
2022-02-08 11:16:56 +0300

    feat:find the product of array except self

--- src/main/java/com/nicky/PracticeQuestions/Arrays/ArrayProduct.java
+++ src/main/java/com/nicky/PracticeQuestions/Arrays/ArrayProduct.java
@@ -1,2 +1,21 @@
-package com.nicky.PracticeQuestions.Arrays;public class ArrayProduct {
+package com.nicky.PracticeQuestions.Arrays;
+
+import java.util.Arrays;
+
+public class ArrayProduct {
+    public int[] productExceptSelf(int[] nums) {
+        int [] product = new int[nums.length];
+        for (int i=0; i< nums.length; i++){
+            product[i]=nums[0];
+            product[i]=product[i]*nums[i];
+        }
+        return product;
+    }
+
+
+    public static void main(String[] args) {
+        ArrayProduct arrayProduct = new ArrayProduct();
+        int [] nums={1,2,3,4};
+        System.out.println(Arrays.toString(arrayProduct.productExceptSelf(nums)));
+    }
 }
