90cb01904855697e69c50d4cbac4b036d1182c8b
NickyGuants <nicholasguantai528@gmail.com>
2022-02-10 06:11:03 +0300

    feat:implement tree and insert element

--- src/main/java/com/nicky/BinaryTrees/Tree.java
+++ src/main/java/com/nicky/BinaryTrees/Tree.java
@@ -1,4 +1,43 @@
 package com.nicky.BinaryTrees;
 
 public class Tree {
+    private Node root;
+
+    private class Node{
+        private int value;
+        private Node left;
+        private Node right;
+
+        public Node(int value) {
+            this.value = value;
+        }
+    }
+
+    public void insert(int value){
+        //find the position where to insert
+        var node = new Node(value);
+        if(root == null){
+            root=node;
+            return;
+        }
+        var current = root;
+
+        while (true){
+            if(value < current.value){
+                if (current.left == null){
+                    current.left=node;
+                    break;
+                }
+                current=current.left;
+            }else{
+                if (current.right==null){
+                    current.right=node;
+                    break;
+                }
+                current=current.right;
+            }
+        }
+    }
+
+
 }
