27913bd9d9da23d40531ebde4bb0f62132cbb137
NickyGuants <nicholasguantai528@gmail.com>
2022-02-10 06:12:27 +0300

    feat:test the insert and find methods

--- src/main/java/com/nicky/BinaryTrees/Main.java
+++ src/main/java/com/nicky/BinaryTrees/Main.java
@@ -1,4 +1,18 @@
 package com.nicky.BinaryTrees;
 
 public class Main {
+    public static void main(String [] args){
+        Tree tree = new Tree();
+        tree.insert(7);
+        tree.insert(4);
+        tree.insert(9);
+        tree.insert(1);
+        tree.insert(6);
+        tree.insert(8);
+        tree.insert(10);
+
+        System.out.println(tree.find(13));
+
+        System.out.println(tree);
+    }
 }
