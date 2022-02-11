4294dad4fcd8f476a064b4c4df7a21949271fd81
NickyGuants <nicholasguantai528@gmail.com>
2022-02-11 08:45:48 +0300

    feat:attempt valid palindrome problem

---
+++ src/main/java/com/nicky/PracticeQuestions/Arrays/ValidPalindrome.java
@@ -0,0 +1,14 @@
+package com.nicky.PracticeQuestions.Arrays;
+
+public class ValidPalindrome {
+    public static boolean isPalindrome(String s){
+        //convert s to lowercase and remove special characters, spaces, and numbers
+        for (int i= 0; i<s.toCharArray().length; i++){
+        }
+        return false;
+    }
+
+    public static void main(String [] args){
+
+    }
+}
