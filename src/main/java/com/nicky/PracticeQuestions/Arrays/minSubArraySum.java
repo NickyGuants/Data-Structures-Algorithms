cf8aed629eb0b1c446d10c9a540c15f1835ff043
NickyGuants <nicholasguantai528@gmail.com>
2022-02-11 17:21:47 +0300

    feat:solve min length sub-array

--- src/main/java/com/nicky/PracticeQuestions/Arrays/minSubArraySum.java
+++ src/main/java/com/nicky/PracticeQuestions/Arrays/minSubArraySum.java
@@ -3,7 +3,7 @@ package com.nicky.PracticeQuestions.Arrays;
 public class minSubArraySum {
     //Brute force approach
     public static int minSubArrayLen(int target, int[] nums){
-        int min_len = 0;
+        int min_len = Integer.MAX_VALUE;
         for(int i=0; i< nums.length; i++){
             int current_sum = nums[i];
             if (current_sum >=target) return 1;
@@ -17,10 +17,24 @@ public class minSubArraySum {
         return min_len;
     }
 
+    public static int minLen(int target, int[] nums){
+        int min_len = Integer.MAX_VALUE;
+        int windowStart=0;
+        int current_sum=0;
 
+        for (int windowEnd=0; windowEnd<nums.length; windowEnd++){
+            current_sum+=nums[windowEnd];
+            while (current_sum>=target){
+                min_len=Math.min(min_len, windowEnd-windowStart+1);
+                current_sum -=nums[windowStart++];
+            }
+        }
+        return min_len == Integer.MAX_VALUE ? 0: min_len;
+    }
 
     public static void main(String [] args){
-        int [] nums={3,1,1,2,3,4};
-        System.out.println(minSubArrayLen(5, nums));
+        int [] nums={1,2,2};
+        //System.out.println(minSubArrayLen(5, nums));
+        System.out.println(minLen(6, nums));
     }
 }
