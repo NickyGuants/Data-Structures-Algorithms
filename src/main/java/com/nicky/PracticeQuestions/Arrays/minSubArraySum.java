d13a4d3ec1225568a99e3f6362b3c5ac787b7d71
NickyGuants <nicholasguantai528@gmail.com>
2022-02-11 10:43:43 +0300

    feat:find min length of subarray using brute force

--- src/main/java/com/nicky/PracticeQuestions/Arrays/minSubArraySum.java
+++ src/main/java/com/nicky/PracticeQuestions/Arrays/minSubArraySum.java
@@ -1,12 +1,26 @@
 package com.nicky.PracticeQuestions.Arrays;
 
 public class minSubArraySum {
+    //Brute force approach
     public static int minSubArrayLen(int target, int[] nums){
-       return 0;
+        int min_len = 0;
+        for(int i=0; i< nums.length; i++){
+            int current_sum = nums[i];
+            if (current_sum >=target) return 1;
+            for (int j=i+1; j<nums.length; j++){
+                current_sum +=nums[j];
+                if (current_sum >= target){
+                    min_len=j-i+1;
+                }
+            }
+        }
+        return min_len;
     }
 
+
+
     public static void main(String [] args){
-        int [] nums={2,3,1,2,4,3};
-        System.out.println(minSubArrayLen(7, nums));
+        int [] nums={3,1,1,2,3,4};
+        System.out.println(minSubArrayLen(5, nums));
     }
 }
