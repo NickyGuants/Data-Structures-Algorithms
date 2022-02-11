d64538de8bcf187eba94ef8e12acd0c2a22255cb
NickyGuants <nicholasguantai528@gmail.com>
2022-02-11 08:47:49 +0300

    feat: testing tree traversals

--- src/main/java/com/nicky/BinaryTrees/Main.java
+++ src/main/java/com/nicky/BinaryTrees/Main.java
@@ -1,6 +1,7 @@
 package com.nicky.BinaryTrees;
 
 public class Main {
+
     public static void main(String [] args){
         Tree tree = new Tree();
         tree.insert(7);
@@ -11,8 +12,28 @@ public class Main {
         tree.insert(8);
         tree.insert(10);
 
-        System.out.println(tree.find(13));
+//        System.out.println(tree.find(13));
+//        System.out.println(tree);
+        //System.out.println(factorial(4));
+        //tree.traversePreOrder();
+        //tree.traverseInOrder();
+//        tree.traversePostOrder();
 
-        System.out.println(tree);
+        System.out.println(tree.height());
+
+    }
+
+    public static int factorial(int n){
+        //using a loop
+        int factorial = 1;
+//        for (int i=n; i>1; i--){
+//            factorial*=i;
+//        }
+
+        //using recursion (n* (n-1)!
+        //base condition
+        if(n==0) return 1;
+        factorial =n * factorial(n-1);
+        return factorial;
     }
 }
