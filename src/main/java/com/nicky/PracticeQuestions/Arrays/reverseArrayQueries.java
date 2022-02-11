d56946ec7ed55186212c07e406bbd4ebf5628dc3
NickyGuants <nicholasguantai528@gmail.com>
2022-02-11 17:20:41 +0300

    feat: reverse array multiple times

--- src/main/java/com/nicky/PracticeQuestions/Arrays/reverseArrayQueries.java
+++ src/main/java/com/nicky/PracticeQuestions/Arrays/reverseArrayQueries.java
@@ -1,2 +1,30 @@
-package com.nicky.PracticeQuestions.Arrays;public class reverseArrayQueries {
+package com.nicky.PracticeQuestions.Arrays;
+
+import java.util.Arrays;
+
+public class reverseArrayQueries {
+    public static int[] performOperations(int[] arr, int[][] operations) {
+        for(int i=0; i<operations.length; i++){
+            reverse(arr,operations[i][0] , operations[i][1]);
+        }
+        return arr;
+    }
+
+    private static void reverse(int[] nums, int start, int end){
+        int temp;
+        while(start < end){
+            temp= nums[start];
+            nums[start]= nums[end];
+            nums[end]=temp;
+            start++;
+            end--;
+        }
+    }
+
+    public static void main(String [] args){
+        int[] arr= {1,2,3};
+        int[][] operations= {{0,2}, {1,2}, {0,2}};
+
+        System.out.println(Arrays.toString(performOperations(arr, operations)));
+    }
 }
