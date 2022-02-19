05d89000eaed1ec9de3018e74fbc388e92cef578
NickyGuants <nicholasguantai528@gmail.com>
2022-02-19 09:42:54 +0300

    implement auto-completion

--- src/main/java/com/nicky/tries/Main.java
+++ src/main/java/com/nicky/tries/Main.java
@@ -4,11 +4,17 @@ public class Main {
     public static void main(String [] args){
         Trie trie = new Trie();
         trie.insertUsingHashTable("car");
+        trie.insertUsingHashTable("card");
         trie.insertUsingHashTable("care");
+        trie.insertUsingHashTable("careful");
+        trie.insertUsingHashTable("egg");
 
         //trie.traverse();
-        trie.remove(null);
-        System.out.println(trie.contains("car"));
-        System.out.println(trie.contains("car"));
+//        trie.remove(null);
+//        System.out.println(trie.contains("car"));
+//        System.out.println(trie.contains("car"));
+
+        var words = trie.findWords("car");
+        System.out.println(words);
     }
 }
