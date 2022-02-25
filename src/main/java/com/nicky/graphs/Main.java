e8e620307cc1f06f7182c7d5f91f3a824c2a697a
NickyGuants <nicholasguantai528@gmail.com>
2022-02-25 07:42:02 +0300

    feat:create a graph and implement add and remove methods

--- src/main/java/com/nicky/graphs/Main.java
+++ src/main/java/com/nicky/graphs/Main.java
@@ -1,4 +1,19 @@
 package com.nicky.graphs;
 
 public class Main {
+    public static void main(String [] args){
+        Graph graph = new Graph();
+        graph.addNode("A");
+        graph.addNode("B");
+        graph.addNode("C");
+
+        graph.addEdge("A", "B");
+        graph.addEdge("A", "C");
+
+        graph.addEdge("B", "C");
+
+        graph.removeNode("C");
+
+        graph.print();
+    }
 }
