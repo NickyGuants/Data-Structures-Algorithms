2a95ebf4d2b9970f3c9e02e5b671dfec6c1efc8c
NickyGuants <nicholasguantai528@gmail.com>
2022-03-01 05:49:27 +0300

    feat:longest sub-string without repeating chars:sliding window

--- src/main/java/com/nicky/PracticeQuestions/Arrays/LongestSubString.java
+++ src/main/java/com/nicky/PracticeQuestions/Arrays/LongestSubString.java
@@ -1,16 +1,24 @@
 package com.nicky.PracticeQuestions.Arrays;
 
+import java.util.HashSet;
+
 public class LongestSubString {
     public static int lengthOfLongestSubstr(String s){
-        int max_length = Integer.MIN_VALUE;
+        int max_length = 0;
         int window_start=0;
-        int windowLen=0;
-        for (int windowEnd=0; windowEnd<s.length(); windowEnd++){
-            windowLen=windowEnd-window_start;
-            max_length=Math.max(windowLen, max_length);
+        int window_end=0;
+        HashSet<Character> uniqueset = new HashSet<>();
+        while (window_end<s.length()){
+            if (!uniqueset.contains(s.charAt(window_end))){
+                uniqueset.add(s.charAt(window_end));
+                window_end++;
+                max_length=Math.max(max_length, window_end-window_start);
+            }else{
+                uniqueset.remove(s.charAt(window_start));//0(1)
+                window_start++;
+            }
         }
-
-        return max_length;
+      return max_length;
     }
 
     public static void main(String [] args){
