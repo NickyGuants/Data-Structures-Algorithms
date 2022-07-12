fa0e8a55114fd7d0a5c4eb31f3603bd845e3ac4d
NickyGuants <nicholasguantai528@gmail.com>
2022-07-12 05:03:59 +0300

    leetcode:check if string is a palindrome

--- src/main/java/com/nicky/PracticeQuestions/Arrays/ValidPalindrome.java
+++ src/main/java/com/nicky/PracticeQuestions/Arrays/ValidPalindrome.java
@@ -3,12 +3,22 @@ package com.nicky.PracticeQuestions.Arrays;
 public class ValidPalindrome {
     public static boolean isPalindrome(String s){
         //convert s to lowercase and remove special characters, spaces, and numbers
-        for (int i= 0; i<s.toCharArray().length; i++){
+        int start=0;
+        int end = s.length()-1;
+        while(start < end){
+            if (s.charAt(start) != s.charAt(end)){
+                return false;
+            }
+            start++;
+            end--;
         }
-        return false;
+        return true;
     }
 
     public static void main(String [] args){
-
+        String str ="bccb";
+        String str1 ="babc";
+        System.out.println(isPalindrome(str));
+        //System.out.println(isPalindrome(str1));
     }
 }
